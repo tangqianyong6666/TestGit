9886bbc7759ed60fbf0efbe6efd662effe79f56e
tangqianyong6666 <1226201656tang>
2021-08-01 16:35:46 +0800

    da

---
+++ src/Object/JDBC/Test.java
@@ -0,0 +1,11 @@
+package Object.JDBC;
+
+/**
+ * @author 摩西摩西
+ * @date 2021/8/1  16:31
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("aaaaa");
+    }
+}
